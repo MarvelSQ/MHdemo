a175d79df3a956e4e7474252eb8f6ab490ccea11
MarvelSQ <sun1993hlj@hotmail.com>
2016-10-30 12:54:23 +0800

    Initial commit

---
+++ src/main/java/com/bmeit/MHdemoApplication.java
@@ -0,0 +1,12 @@
+package com.bmeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MHdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MHdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bmeit/MHdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bmeit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MHdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
